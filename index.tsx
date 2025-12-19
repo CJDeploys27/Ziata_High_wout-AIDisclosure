8eb7969f5b8e91f2312ed51d41adab5384292a76
CJDeploys27 <cj@insightfuldeployment.io>
2025-12-19 14:05:12 -0500

    Move BACKEND_API_URL declaration outside conditional

--- index.tsx
+++ index.tsx
@@ -6,8 +6,9 @@ import App from './App';
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
-const BACKEND_API_URL = 'https://ziata-high-wout-aidisclosure-771555879264.europe-west1.run.app';
 }
+
+const BACKEND_API_URL = 'https://ziata-high-wout-aidisclosure-771555879264.europe-west1.run.app';
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
