cd7e601f6ae08b456f57cb2e5a92085e0ed85649
CJDeploys27 <cj@insightfuldeployment.io>
2025-12-19 18:01:36 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- ADD THIS LINE! This is the magic fix.
+  base: '/', 
   build: {
     outDir: 'dist',
     sourcemap: false,
