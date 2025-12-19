63c6e57a8db5d32f52419fad429696ca55980623
CJDeploys27 <cj@insightfuldeployment.io>
2025-12-19 14:28:06 -0500

    Add base path configuration to vite.config.ts
    Set base path for Vite build.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // <--- ADD THIS LINE! This is the magic fix.
   build: {
     outDir: 'dist',
     sourcemap: false,
